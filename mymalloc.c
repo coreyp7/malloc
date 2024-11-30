ad3fadf96e4e99744e8ed5d4dff3a0b3fcd91305
Corey Pierce <coreypierce11@gmail.com>
2024-11-30 13:19:35 -0500

    Fix naming convention of some stuff that was off

--- mymalloc.c
+++ mymalloc.c
@@ -5,47 +5,41 @@
 #include <stdio.h>
 #include "mymalloc.h"
 
-struct blockMeta {
+struct BlockMeta {
 	size_t size; // size of this block of memory
-	struct blockMeta *next; // next memory block for this program
+	struct BlockMeta *next; // next memory block for this program
 	int free; // is this block free or taken already?
 	int magic; // 'For debugging only'
 };
 
-typedef struct blockMeta blockMeta;
+typedef struct BlockMeta BlockMeta;
 
-blockMeta *find_free_block(blockMeta **last, size_t size);
-blockMeta *requestSpace(blockMeta* last, size_t size);
-blockMeta *get_memblock(void* ptr);
+BlockMeta *_find_free_block(BlockMeta **last, size_t size);
+BlockMeta *_requestSpace(BlockMeta* last, size_t size);
+void print_heap_visualization();
 void *my_malloc(size_t size);
 void my_free(void* ptr);
 
-#define META_SIZE sizeof(blockMeta)
+#define META_SIZE sizeof(BlockMeta)
 
 void *globalBase = NULL; // the head of our linked list
 
-/*
-int main(){
-	printf("sizeof(blockMeta):%zu", META_SIZE);
-}
-*/
-
 void *my_malloc(size_t size){
-	blockMeta *block;
+	BlockMeta *block;
 	
 	// I always will go into this as long as I call this.
 	if(globalBase == NULL){
-		block = requestSpace(NULL, size);	
+		block = _requestSpace(NULL, size);	
 		globalBase = block;
 	} else { // will go in any call past first one
 
 		// Search our linked list for space big enough.
 		// If it doesn't exist, then request for more
 		// space from the OS.
-		blockMeta *last = globalBase;	
-		block = find_free_block(&last, size);
+		BlockMeta *last = globalBase;	
+		block = _find_free_block(&last, size);
 		if(!block){
-			block = requestSpace(last, size);
+			block = _requestSpace(last, size);
 			if(!block){
 				return NULL;
 			}
@@ -57,9 +51,9 @@ void *my_malloc(size_t size){
 			// if this block is big enough to accomodate a new block, let's split it.
 			// ( + sizeof(unsigned char) is to set the minimum size for a mem block
 			// to be 1 byte.
-			if(block->size > size + sizeof(blockMeta) + sizeof(unsigned char)){
+			if(block->size > size + sizeof(BlockMeta) + sizeof(unsigned char)){
 				printf("Splitting block at %p since its really big (memory actually starts at %p).\n", block, block+1);
-				blockMeta* newBlock = (blockMeta*)block + 1 + size;
+				BlockMeta* newBlock = (BlockMeta*)block + 1 + size;
 				size_t sizeOfNewBlock = block->size - size - META_SIZE;
 				newBlock->free = 1;
 				newBlock->size = sizeOfNewBlock;
@@ -83,6 +77,8 @@ void *my_malloc(size_t size){
 		}
 	}
 	printf("Memory block starting at %p has been returning to caller requesting %zu bytes.\n", block + 1, size);
+
+	print_heap_visualization();
 	return block+1;	
 }
 
@@ -91,7 +87,7 @@ void my_free(void* ptr){
 		return;
 	}
 
-	blockMeta* block = get_memblock(ptr);
+	BlockMeta* block = (BlockMeta*)ptr -1;
 	block->free = 1;
 }
 
@@ -103,8 +99,8 @@ void my_free(void* ptr){
 // Hey, find me a free block of space.
 // Return a pointer to the memory block metadata.
 // Returns null if one doesn't exist yet.
-blockMeta *find_free_block(blockMeta **last, size_t size){
-	blockMeta *curr = globalBase;
+BlockMeta *_find_free_block(BlockMeta **last, size_t size){
+	BlockMeta *curr = globalBase;
 	// continue searching until we find a free block
 	// big enough for us.
 	while (curr && !(curr->free && curr->size >= size)){
@@ -115,8 +111,8 @@ blockMeta *find_free_block(blockMeta **last, size_t size){
 }
 
 // NOTE: first time you call this, set last to NULL.
-blockMeta *requestSpace(blockMeta* last, size_t size){
-	blockMeta *block;
+BlockMeta *_requestSpace(BlockMeta* last, size_t size){
+	BlockMeta *block;
 	block = sbrk(0);
 	void *request = sbrk(size + META_SIZE);
 	assert((void*)block == request); // Not thread safe?
@@ -135,11 +131,8 @@ blockMeta *requestSpace(blockMeta* last, size_t size){
 }
 
 
-// given a ptr of beginning of mem block; 
-// get the memblock struct for this block of memory.
-blockMeta *get_memblock(void* ptr){
-	return (blockMeta*)ptr -1;
+
+void print_heap_visualization(){
+	printf("heap visual call\n");
 }
-
-
 
