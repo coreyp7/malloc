c2b8a38882689d28caf5a912b90b0bbf2e4028a9
Corey Pierce <coreypierce11@gmail.com>
2024-12-15 13:19:44 -0500

    Initial MemoryTable commit

--- gc/MemoryTable.c
+++ gc/MemoryTable.c
@@ -1,48 +1,59 @@
-/*
-typedef struct MemoryBlock {
-	size_t size;
-	// To the actual memory block this represents
-	// Also the key in the hashtable; useful beause of linear probing
-	void* ptr; 
-
-	// all just stuff that may be convenient later
-	struct MemoryBlock* next;
-	struct MemoryBlock* prev;
-	int free;
-	int marked; 
-};
-
-typedef struct MemoryTable {
-	size_t size; // of the underlying array I believe
-	MemoryBlock* start; // start of arr	
-};
-
-
-MemoryBlock* 
-*/
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 // Using struct since it'll be a struct when I actually use it later.
-typedef struct Number {
-	uintptr_t num;
+typedef struct {
 	// linear probing, so we need to know what they key is
 	// so we can match with it when linearly searching for value.
-	int key;
-};
+	uintptr_t key;
 
-Number createNumber(uintptr_t num){
-	Number newNum;
-	newNum.num = num;
-	// TODO: set key with hasing function
-	return newNum;
+	// ptr to actual memory block this represents
+	void* ptr;
+} MemoryBlock;
+
+
+typedef struct {
+	size_t size; // of underlying array, int bytes not elements
+	MemoryBlock* head;	
+} HashTable;
+
+size_t hash(void *ptr)
+{
+    return (((uintptr_t)ptr) >> 3) % 557;
 }
 
-typedef struct HashTable {
-	size_t size; // of underlying array, int bytes not elements
-	Number* head;	
-};
+void* allocate(size_t size, HashTable* table){
+	// Call malloc and get memory from os.
+	MemoryBlock memBlock;
+	memBlock.ptr = malloc(size);
+	memBlock.key = (uintptr_t)memBlock.ptr;
+	
+	// Use the address of the ptr returned from malloc
+	// to put it into our map with our hashing function.
+	table->head[hash(memBlock.ptr)] = memBlock;	
+	printf("Put %p into index %li\n", memBlock.ptr, hash(memBlock.ptr));
+	
+	// TODO: handle collisions
+	return memBlock.ptr;
+}
 
-
+void* deallocate(void* ptr, HashTable* table){
+	// Find the element and free
+	MemoryBlock* block = &table->head[hash(ptr)];
+	free(block->ptr);
+	block->key = -1;
+	free(block); // this might be wrong
+}
 
 int main(){
+	HashTable table;
+	table.head = malloc(557 * sizeof(MemoryBlock));	
+	table.size = 557 * sizeof(MemoryBlock);
 
+	int* newInt = allocate(sizeof(int), &table);
 }
